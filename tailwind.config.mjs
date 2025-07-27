99fd22ed503a4223fa0b86e12659f3f8d330b76c
aaai2026_19745 <aaai2026_19745@126.com>
2025-07-27 13:54:30 +0800

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // 使用类名控制的暗黑模式
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
